c6f8b94765eeaba7daf2ac6ecb650fc22fa462c7
Dierk Koenig <dierk.koenig@canoo.com>
2022-03-07 10:34:58 +0100

    en after week 3

--- week3/lambda/lambda.js
+++ week3/lambda/lambda.js
@@ -1,4 +1,20 @@
+const id    = x =>      x;
+const konst = x => y => x;
+const snd   = x => y => y;
 
+const T = konst;
+const F = snd;
+
+const and = p => q => p (q) (p);
+const or  = p => q => p (p) (q);
+
+const Pair      = x => y => f => f(x)(y);
+const firstname = konst;
+const lastname  = snd;
+
+const Left   = x => f => g => f(x);
+const Right  = x => f => g => g(x);
+const either = id;
 
 
 // ----- special -----
--- week3/lambda/lambdaTest.js
+++ week3/lambda/lambdaTest.js
@@ -1,36 +1,36 @@
 // requires lambda.js
 
 
-// let ok = [];
-//
-// // id
-// ok.push( id(1) === 1 );
-// ok.push( id(id) === id );
-//
-// // konst
-// ok.push( konst(42)(0) === 42 );
-// ok.push( konst(42)(1) === 42 );
-// ok.push( konst(42)(null) === 42 );
-//
-// // kite
-// ok.push( snd(null)(42) === 42 );
-//
-// // true
-//
-// ok.push( T(1)(0) === 1 );
-// ok.push( F(1)(0) === 0 );
-//
+let ok = [];
+
+// id
+ok.push( id(1)  === 1 );
+ok.push( id(id) === id );
+
+// konst
+ok.push( konst(42)(0)    === 42 );
+ok.push( konst(42)(1)    === 42 );
+ok.push( konst(42)(null) === 42 );
+
+// kite
+ok.push( snd(null)(42) === 42 );
+
+// true
+
+ok.push( T(1)(0) === 1 );
+ok.push( F(1)(0) === 0 );
+
 // // and
-// ok.push( and(F)(F) === F );
-// ok.push( and(T)(F) === F );
-// ok.push( and(F)(T) === F );
-// ok.push( and(T)(T) === T );
+ok.push( and(F)(F) === F );
+ok.push( and(T)(F) === F );
+ok.push( and(F)(T) === F );
+ok.push( and(T)(T) === T );
 //
 // // or
-// ok.push( or(F)(F) === F );
-// ok.push( or(T)(F) === T );
-// ok.push( or(F)(T) === T );
-// ok.push( or(T)(T) === T );
+ok.push( or(F)(F) === F );
+ok.push( or(T)(F) === T );
+ok.push( or(F)(T) === T );
+ok.push( or(T)(T) === T );
 
 // flip
 // flip(f)(x)(y) = f(y)(x)
@@ -41,9 +41,9 @@
 //
 // // Pair
 //
-// const dierk = Pair("Dierk")("König"); // immutable
-// ok.push( dierk(firstname) === "Dierk");
-// ok.push( dierk(lastname)  === "König");
+const dierk = Pair("Dierk")("König"); // immutable
+ok.push( dierk(firstname) === "Dierk");
+ok.push( dierk(lastname)  === "König");
 //
 // const tdierk = Triple("Dierk")("König")(50); // immutable
 // ok.push( tdierk(tfirstname) === "Dierk");
@@ -73,7 +73,7 @@ const safeDiv = num => divisor =>
     ? Left("schlecht!")
     : Right(num / divisor);
 
-either( safeDiv(1)(0)  )
+safeDiv(1)(1)
       ( x => console.error(x))
       ( x => console.log(x));
 
